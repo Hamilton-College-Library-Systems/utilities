ddfae32373ba65ff19dddcfe376efeb497d6de2a
Peter MacDonald <petermacdonald88@gmail.com>
2018-08-13 11:38:05 -0400

    Update folders2zips-2018-08-13.sh

--- folders2zips-2018-08-13.sh
+++ folders2zips-2018-08-13.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #######################
 # Zips each directory into a separate zip file
-# Be sure to creatg a "/zips" folder parallel to the HOME_DIR.
+# Be sure to create a "/zips" folder parallel to the HOME_DIR.
 #######################
 HOME_DIR="/home2/lisham/diglib-15/LibSpace/CDM-Migration/cwl/IMI-Civil-War-TEI/spe-civ-adm/obj_files-obsolete/multiple-images"
 cd $HOME_DIR
